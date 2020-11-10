2a3f33e9c079aa7343d0ee291256f8d9b86faafd
thanapato <thanapat.skt@gmail.com>
2020-11-10 18:16:57 +0700

    Version 3

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -1,28 +1,25 @@
 package data;
 
 import base.Bike;
+import base.Location;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
 
 public class ToyData {
 
     public static Bike[] getBikes() {
-        Bike[] bikes = new Bike[3];
-        // your code
-
+      
+         Bike[] bikes = new Bike[3];
+         bikes[0]=new Bike("BMW",1000);
+         bikes[1]=new Bike("Honda",500);
+         bikes[2]=new Bike("Toyota",250);
+        
         return bikes;
     }
 
     public static Person[] getPerson() {
         Person[] person = new Person[3];
-       
-//        Person a = new Person("Thanapat","Trairatpanasan") ;
-//        Person b = new Person("Kim","Dekhier") ;
-//        Person c = new Person("Big","Onnairkernnnnn") ;
-//        person [1] = a;
-//        person [2] = b;
-//        person [3] = c;
-        
         person [1] = new Person("Thanapat","Trairatpanasan") ;
         person [2] = new Person("Kim","Dekhier") ;
         person [3] = new Person("Big","Onnairkernnnnn") ;
@@ -31,7 +28,17 @@ public class ToyData {
 
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
-        // your code
+        
+        parcels[0]= new Parcel (60,ParcelStatus.DELIVERED,new Location(0, 0));
+        parcels[1]= new Parcel (60,ParcelStatus.READY_TO_SHIP,new Location(0, 0));
+        parcels[2]= new Parcel (60,ParcelStatus.SHIPPING,new Location(0, 0));
+        parcels[3]= new Parcel (60,ParcelStatus.DELIVERED,new Location(0, 0));
+        parcels[4]= new Parcel (60,ParcelStatus.READY_TO_SHIP,new Location(0, 0));
+        parcels[5]= new Parcel (60,ParcelStatus.SHIPPING,new Location(0, 0));
+        parcels[6]= new Parcel (60,ParcelStatus.DELIVERED,new Location(0, 0));
+        parcels[7]= new Parcel (60,ParcelStatus.READY_TO_SHIP,new Location(0, 0));
+        parcels[8]= new Parcel (60,ParcelStatus.SHIPPING,new Location(0, 0));
+        parcels[9]= new Parcel (60,ParcelStatus.DELIVERED,new Location(0, 0));
 
         return parcels;
     }
